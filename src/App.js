5c83906d786803f9f8f5a3cdcc85688b3c8414aa
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2021-11-22 05:09:17 +1100

    added styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,60 @@
+import react, { useState } from "react";
+
+import "./css/main.css";
+
+
+
 function App() {
+
+  const [total, setTotal] = useState(0);
+
+
+  const calculateTotal = () => {
+    
+  }
+
   return (
     <div className="App">
-      
+      <main>
+        <div id="topBar" className="blackBackground">
+          <h1>
+            What to tip?
+          </h1>
+        </div>
+        
+        <div id="mainContent">
+          
+          <div className="inputField">
+            <label htmlFor="total">
+              Total: 
+            </label>
+            <input type="number" id="total" />
+          </div>
+                              
+          <div className="inputField">
+            <label htmlFor="people">
+              Number of people: 
+            </label>
+            <input type="number" id="people" />
+          </div>
+          
+          <div className="inputField">
+            <label htmlFor="tip">
+              Tip %: 
+            </label>
+            <input type="number" id="tip" />
+          </div>
+        </div>
+        <div id="owing">
+          <hr />
+          <h3>
+            Price per person: ${total}
+          </h3>
+        </div>
+        <footer className="blackBackground">
+          &copy; Therealcoolpup
+        </footer>
+      </main>
     </div>
   );
 }
